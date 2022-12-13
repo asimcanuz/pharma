0d49d1dbec50ffee6c1765176b898ad05d6a4221
asimcanuz <asimcanuz@gmail.com>
2022-12-13 22:25:11 +0300

    update for layout pages

--- admin-panel/client/src/pages/Layout/index.js
+++ admin-panel/client/src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Topbar from "../../components/Topbar";
 
@@ -6,7 +7,15 @@ export default function Layout(params) {
     <main className="content">
       {/* TOPBAR */}
       <Topbar />
-      <Outlet />
+      <Box m="20px">
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Outlet />
+        </Box>
+      </Box>
     </main>
   );
 }
